9ed914ea40923539730a77f33db7c44dfb9c5933
tkaburagi <tkaburagi@kabu-2.local>
2016-11-10 12:58:35 +0900

    PEZ

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Users;
 import com.example.repo.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -22,6 +27,14 @@ public class UserController {
 	public Users getOneUser(@RequestParam("name") String name) {
 //		return repo.findAll();
 		return repo.findByName(name);
+	}
+	
+	@RequestMapping("/getinstance")
+	public String getInstance() throws JsonProcessingException, IOException {
+		String vcap = System.getenv("VCAP_APPLICATION");
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode vcap_app = mapper.readTree(vcap);
+		return vcap_app.get("instance_index").asText();
 	}
 
 	@RequestMapping("/v")
